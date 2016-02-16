d35919509b7282b067d4a1780f5e0093c36cd374
Sergey Romanov <xxsmotur@gmail.com>
2016-02-17 00:23:29 +0500

    Update

--- httpattack.go
+++ httpattack.go
@@ -32,3 +32,20 @@ func (attack *Attack) run(workers *sync.WaitGroup){
 	workers.Add(1)
 	defer workers.Done()
 }
+
+func buildHttpRequest(httpAction HttpAction, sessionMap map[string]string) *http.Request {
+    var req *http.Request
+    var err error
+    if httpAction.Body != "" {
+        reader := strings.NewReader(SubstParams(sessionMap, httpAction.Body))
+        req, err = http.NewRequest(httpAction.Method, SubstParams(sessionMap, httpAction.Url), reader)
+    } else {
+        req, err = http.NewRequest(httpAction.Method, SubstParams(sessionMap, httpAction.Url), nil)
+    }
+    if err != nil {
+        log.Fatal(err)
+    }
+    req.Header.Add("Accept", httpAction.Accept)
+
+    return req
+}
