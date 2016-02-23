4cf58707bba0a9b13a6405d5b59f711e845bde75
Sergey Romanov <xxsmotur@gmail.com>
2016-02-23 23:57:25 +0500

    Add struct for result

--- result.go
+++ result.go
@@ -4,6 +4,10 @@ import (
    "net/http"
 )
 
+type Response struct {
+	Result  string
+}
+
 func provideResponse(method string, client *http.Client) error {
 	/*_, err := http.NewRequest(method)
 	if err != nil {
